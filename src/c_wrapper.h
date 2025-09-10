e58a9fb759d80cdfcbba2b164a05b22b4231334e
Philipp Edelmann <pedelmann@lanl.gov>
2025-09-10 16:48:16 -0600

    fix C wrappers
    This makes the header extern "C" not the source and fixes many small
    errors and omissions in the wrapper functions.

--- src/c_wrapper.h
+++ src/c_wrapper.h
@@ -1,35 +1,31 @@
 #ifndef HFLUX_C_WRAPPER_H_
 #define HFLUX_C_WRAPPER_H_
 
-void hflux_init(
-    const int nR_data,
-    const int nZ_data,
-    const int nfields,
-    const int nphi_data,
-    const int nt,
-    const double R0,
-    const double Z0,
-    const double dR,
-    const double dZ
-    void ** fi);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-void hflux_compute_poincare(
-    void* fi,
-    const double r0,
-    const double dr,
-    const int n_r
-    const int n_theta,
-    const double* poincare_data);
+  void hflux_init(const int nR_data, const int nZ_data, const int nfields,
+                  const int nphi_data, const int nt, const double R0,
+                  const double Z0, const double dR, const double dZ, void **fi);
 
-void hflux_field_eval(
-    void* fi,
-    double R,
-    double phi,
-    double Z,
-    double t,
-    double* B,
-    double* curlB,
-    double* dBdR,
-    double* dBdZ,
-    double* E);
+  void hflux_interpolate(void *fi, double *raw_field_data);
+
+  void hflux_compute_poincare(void* fi,
+                              const double r0,
+                              const double dr,
+                              const int n_r,
+                              const int n_theta,
+                              const double* poincare_data);
+
+  void hflux_field_eval(void *fi, const int N, const double *R_mesh,
+                        const double *phi_mesh, const double *Z_mesh,
+                        const double *t_mesh, double *mesh_value);
+
+  void hflux_destroy(void* fi);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
